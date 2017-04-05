eae716a23680c8b4962a10619d6731a11353ffcb
Administrator <18249028972@163.com>
2017-04-05 21:13:48 +0800

    Initial commit

---
+++ app/src/main/java/com/namewu/customviewlistdemo/main/MainActivity.java
@@ -0,0 +1,18 @@
+package com.namewu.customviewlistdemo.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.namewu.customviewlistdemo.R;
+
+/**
+ * Created by Administrator on 2017/4/3.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
